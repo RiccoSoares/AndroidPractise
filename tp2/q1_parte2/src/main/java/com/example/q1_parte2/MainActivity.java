3efd4fcb629f3a8a375d5522f429b4a334e6cbcc
ricco <riccovcs@terra.com.br>
2022-02-22 20:56:55 -0300

    Implement TP2 Part 2 Q1

---
+++ tp2/q1_parte2/src/main/java/com/example/q1_parte2/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.q1_parte2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorManager;
+import android.hardware.SensorEventListener;
+import android.content.Context;
+import android.content.Intent;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener{
+    TextView output;
+    private SensorManager sensorManager;
+    private SensorEventListener sensorEventListener;
+    private Sensor lightSensor;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        output = (TextView) findViewById(R.id.lightInfo);
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        lightSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+
+        if(lightSensor == null){
+            output.setText("No Light Sensor Found In Your Device");
+            finish();
+        }
+
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent){
+         float lumValue = sensorEvent.values[0];
+         output.setText("Luinosity: " + lumValue);
+
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        sensorManager.registerListener(this, lightSensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        sensorManager.unregisterListener(sensorEventListener);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i){
+
+    }
+}
